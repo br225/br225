7597ddcc8c4123683d81186721790c144c8c240c
br225 <83274375+br225@users.noreply.github.com>
2021-06-12 00:42:01 +0300

    Add files via upload

---
+++ MyProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  MyProtocol.swift
+//  Focaccia
+//
+//  Created by ברוסלי סואעד  on 11/06/2021.
+//
+
+import UIKit
+
+protocol MyProrocol {
+    func imagefetched(image:UIImage)
+    func stringfetched(string: String)
+}
+
